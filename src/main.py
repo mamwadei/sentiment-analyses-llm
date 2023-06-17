a27fc33afe41f630aa5521739ff1e28642059b9b
mahussain <mahussain@nu.edu.sa>
2023-06-17 13:42:49 +0300

    init commit

---
+++ src/main.py
@@ -0,0 +1,90 @@
+from tweety.bot import Twitter
+import re
+import streamlit as st
+from datetime import datetime
+import os
+from sentiment_analyzer import create_dataframe_from_tweets, analyze_sentiment
+from typing import List,Dict
+import random
+import time
+import plotly.graph_objects as go
+
+
+def on_add_author():
+    twitter_handle = st.session_state.twitter_handle
+    if twitter_handle.startswith("@"):
+        twitter_handle = twitter_handle[1:]
+    if twitter_handle in st.session_state.twitter_handles:
+        return
+    tweets_details=[]
+    all_tweets = twitter_client.get_tweets(twitter_handle)
+    if len(all_tweets) ==0:
+        return
+    time.sleep(random.randint(2,2))
+    for i, tweet in enumerate(all_tweets):
+        tweet_detail = twitter_client.tweet_detail(tweet.id)
+        if len(tweet_detail.comments) == 0:
+            continue
+        tweets_details.append(tweet_detail)
+        if i == 3:
+            break
+        time.sleep(random.randint(2,3))
+        
+    st.session_state.twitter_handles[twitter_handle] = all_tweets[0].author.name
+    st.session_state.tweets.extend(tweets_details)
+    st.session_state.author_sentiment[twitter_handle] = analyze_sentiment(twitter_handle,st.session_state.tweets)
+    # st.write(st.session_state.author_sentiment[twitter_handle])
+    
+    pass
+twitter_client = Twitter()
+
+st.set_page_config(
+    layout="wide",
+    page_title="Social Analyzer"
+)
+
+st.title("Social Analyzer")
+
+
+if not "tweets" in st.session_state:
+    st.session_state.tweets = []
+    st.session_state.api_key = ""
+    st.session_state.twitter_handles ={}
+    st.session_state.author_sentiment = {}
+
+os.environ["OPENAI_API_KEY"]= st.session_state.api_key
+    
+col1, col2 = st.columns(2)
+
+with col1:
+    st.text_input("OpenAi API key", type="password", key="api_key", placeholder="sk-.......4567",
+                  help="Get your API key : https://platform.openai.com/account/api-keys")
+    with st.form(key="twitter_handle_form",clear_on_submit=True):
+        st.subheader("Add Twitter Account", anchor=False)
+        st.text_input("Twitter Account", key="twitter_handle" , placeholder="@twitter_username")
+        submit = st.form_submit_button(label="Add Tweets",on_click=on_add_author)
+    if st.session_state.twitter_handles:
+        st.subheader("Twitter Accounts",anchor=False)
+        for handle,name in st.session_state.twitter_handles.items():
+            handle = "@" + handle
+            st.markdown(f"{name}([{handle}](https://twitter.com/{handle}))")
+    st.subheader("Tweets", anchor=False)
+    st.dataframe(create_dataframe_from_tweets(st.session_state.tweets))
+
+labels=["negative","natural","positive"] 
+with col2:
+    for handle,name in st.session_state.twitter_handles.items():
+        author_sentiment=st.session_state.author_sentiment[handle]
+        values= [author_sentiment["negative"],author_sentiment["natural"],author_sentiment["positive"]]
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5)])
+        fig.update_layout(
+            title=f'{name} sentiment chart',
+            title_font_size= 24,
+            title_x=0.2,
+            legend_bgcolor="black",
+            legend_font_color="white",
+            legend_font_size=18,
+            colorway=["yellow","green","red"]
+        )
+        st.plotly_chart(fig, use_container_width=True)
+  
---
+++ src/sentiment_analyzer.py
@@ -0,0 +1,136 @@
+
+from tweety.types import Tweet
+import pandas as pd
+from typing import List, Dict
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import  LLMChain
+from langchain.prompts import  PromptTemplate
+import json
+import re
+from datetime import datetime
+import time
+
+
+
+PROMPT_TEMPLATE ="""
+Please act as a machine learning model trained for perform a supervised learning task, 
+for extract the sentiment of a twitter tweets comments or replies.
+
+You're given tweet comments or replies for this tweet "{tweet_text}":
+
+{comments} 
+
+Tell how positive, natural or negative the comments for that tweet. Use number between 0 and 100, where 0 to 40 negative, 41 to 59 natural and 60 tp 100 is positive for each comment and then provide the count of each category
+Use a JSON using the format: 
+
+"negative": negative count
+"natural": natural count
+"positive": positive count
+
+
+Return just the JSON. Do not explain.
+
+"""
+
+
+
+def clean_tweet(text: str) -> str:
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"www.\S+", "", text)
+    text = re.sub("([@][A-Za-z0-9_]+)|(\w+:\/\/\S+)","", text)
+    return re.sub(r"\s+", " ", text)
+
+
+def create_dataframe_from_tweets(tweets: List[Tweet]) -> pd.DataFrame:
+    rows = []
+    comments=[]
+    for tweet in tweets:
+        clean_text = clean_tweet(tweet.text)
+        if len(clean_text) == 0:
+            continue
+        for comment in tweet.comments:
+            comments.append({
+                "id": comment.id,
+                "text": comment.text,
+                "author": comment.author.username,
+                "date": str(comment.date.date()),
+                "views": comment.views,
+                "created_at": comment.date,
+            }
+            )
+        rows.append({
+            "id": tweet.id,
+            "text": clean_text,
+            "author": tweet.author.username,
+            "date": str(tweet.date.date()),
+            "views": tweet.views,
+            "created_at": tweet.date,
+            "comments": comments,
+        }
+        )
+        
+        comments=[]
+        
+    df = pd.DataFrame(
+        rows, columns=["id", "text", "author", "date", "views", "created_at","comments"]
+    )
+    df.set_index("id", inplace=True)
+    if df.empty:
+        return df
+    df = df[df.created_at.dt.date > datetime.now().date() -
+            pd.to_timedelta("7Day")]
+    return df.sort_values(by="created_at", ascending=False)
+
+def create_list_for_prompt( tweets:List[Tweet],twitter_handle: str):
+    df = create_dataframe_from_tweets(tweets)
+    user_tweets= df[df.author == twitter_handle]
+    if user_tweets.empty:
+        return ""
+    if len(user_tweets) > 100:
+        user_tweets = user_tweets.sample(n=100)
+    tweets_with_comments = []
+    for tweet in tweets :
+        text =""
+        for i, comment in enumerate(tweet.comments):
+            clean_comment = clean_tweet(comment.text)
+            if len(clean_comment) == 0:
+                continue
+            text += f"\n{i+1} - {clean_comment}"
+        tweets_with_comments.append(
+            {
+                "tweet_text": clean_tweet(tweet.text),
+                "comments": text
+            }
+        
+    )
+    
+    return tweets_with_comments
+
+
+def analyze_sentiment(twitter_handle:str, tweets: List[Tweet]) -> Dict[str,int]:
+    full_tweets = create_list_for_prompt(tweets=tweets, twitter_handle= twitter_handle)
+    chat_gpt = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    prompt = PromptTemplate(input_variables=["tweet_text","comments"],template = PROMPT_TEMPLATE)
+    sentiment_chain =LLMChain(llm=chat_gpt,prompt=prompt)
+    user_sentiment = {
+        "negative":0,
+        "natural":0,
+        "positive":0,
+    }
+    print(len(full_tweets))
+    for tweet in full_tweets:
+        response = sentiment_chain(
+            {
+                "tweet_text":tweet["tweet_text"],
+                "comments":tweet["comments"]
+            }
+        )
+        res= json.loads(response["text"])
+      
+        user_sentiment["negative"] += user_sentiment["negative"] + res["negative"]
+        user_sentiment["natural"] += user_sentiment["natural"] + res["natural"]
+        user_sentiment["positive"] += user_sentiment["positive"] + res["positive"]
+        time.sleep(2)
+
+    return user_sentiment
+    
